2dfc1372b0e60947289520cb3b720e6e3b9e60ae
lsjointer16 <75704699+lsjointer16@users.noreply.github.com>
2020-12-08 17:40:52 -0600

    This is a light app
    Have fun!

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    var lightOn = true
+    @IBOutlet weak var lightButton: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        lightOn = !lightOn
+        updateUI()
+    }
+        
+        func updateUI() {
+            if lightOn {
+                view.backgroundColor = .white
+            
+            } else {
+                view.backgroundColor = .black
+            }
+        }
 }
-
